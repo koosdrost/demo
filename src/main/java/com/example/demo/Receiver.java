a958871deb500967de55b17a485d73806036e8a7
koosdrost <koosdrost90@gmail.com>
2019-04-27 15:58:15 +0200

    owasp check

--- src/main/java/com/example/demo/Receiver.java
+++ src/main/java/com/example/demo/Receiver.java
@@ -1,19 +1,15 @@
 package com.example.demo;
 
-import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Component
 public class Receiver {
 
-    List<Email> emails = new ArrayList<>();
-
-    @JmsListener(destination = "mailbox", containerFactory = "myFactory")
-    public void receiveMessage(Email email) {
-        emails.add(email);
-        System.out.println("Received <" + email + ">");
-    }
+//    List<Email> emails = new ArrayList<>();
+//
+//    @JmsListener(destination = "mailbox", containerFactory = "myFactory")
+//    public void receiveMessage(Email email) {
+//        emails.add(email);
+//        System.out.println("Received <" + email + ">");
+//    }
 }
